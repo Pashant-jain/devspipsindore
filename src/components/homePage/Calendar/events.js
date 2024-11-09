e0b52733573ef847574bf64814a0c03eecaaa29c
Prashant Jain <info.prashantjain2002@gmail.com>
2024-11-09 19:51:38 +0530

    fix

---
+++ src/components/homePage/Calendar/events.js
@@ -0,0 +1,58 @@
+
+export const eventList = [
+  {
+      "date": "2024-06-05",
+      "events": [
+          {
+              "title": "World Environment Day",
+              "location": "St. Paul Institute of Professional Studies, Indore",
+              "description": "Day Camp organized by the NSS Committee & Morning Assembly Committee."
+          }
+      ]
+  },
+  {
+      "date": "2024-06-08",
+      "events": [
+          {
+              "title": "Examination Committee Meeting",
+              "location": "St. Paul Institute of Professional Studies, Indore",
+              "description": "Meeting to discuss examination arrangements and policies."
+          }
+      ]
+  },
+  {
+      "date": "2024-06-10",
+      "events": [
+          {
+              "title": "Board of Studies Meeting",
+              "location": "St. Paul Institute of Professional Studies, Indore",
+              "description": "Annual meeting to review the academic curriculum and standards."
+          }
+      ]
+  },
+  {
+      "date": "2024-06-15",
+      "events": [
+          {
+              "title": "International Yoga Week",
+              "location": "St. Paul Institute of Professional Studies, Indore",
+              "description": "Week-long yoga event from 15th to 21st June organized by the NSS & NCC Committee."
+          }
+      ]
+  },
+  {
+      "date": "2024-06-28",
+      "events": [
+          {
+              "title": "Health Camp on Director’s Feast Day",
+              "location": "St. Paul Institute of Professional Studies, Indore",
+              "description": "Health camp organized by the NSS Committee to celebrate the Director’s Feast Day."
+          },
+          {
+              "title": "Inter Department FEF",
+              "location": "Department of Science & Computer Science-B, Indore",
+              "description": "Event hosted by the Science & Computer Science Department."
+          }
+      ]
+  }
+];
