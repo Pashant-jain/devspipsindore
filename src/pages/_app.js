0df06c591666a23168f31cce4013d6a63c8f842f
Prashant Jain <info.prashantjain2002@gmail.com>
2024-11-09 23:16:13 +0530

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.scss";
-import "./../globals.css";
+import './styles.css'
 
 export default function App({ Component, pageProps }) {
   return (
